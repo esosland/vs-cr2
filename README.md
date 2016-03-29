3bc9107bcb7f905bd2cefc200a71b1261c11c82b
Elissa Sosland <esosland@gmail.com>
2016-03-28 20:28:15 -0700

    add JavaScript and JQuery, add README.md, fix HTML

---
+++ README.md
@@ -0,0 +1,19 @@
+# _{Vacation Suggester}_
+
+### _{Parody Vacation Suggester website, Code Review 2, Intro to Programming, Epicodus, Created on 3.28.2016}_
+
+### By _{Elissa Sosland}_
+
+### _{This website uses survey forms to get information from the user and then outputs a humorous vacation suggestion based on the results.}_
+
+### _{Setup}_
+*_Clone git repository_*
+*_Open index.html in Google chrome_*
+*_Or open Github page at_*
+
+### _{Known Bugs: Unknown}_
+
+### _{Technologies Used include JavaScript and JQuery, HTML, and CSS}_
+
+### _{Legal}_
+  **_{Copyright Soslandia 2016}_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,32 @@
 $(document).ready(function() {
+  $("form").submit(function(event) {
+    var score = 0;
+    var result = parseInt($("#preference").val());
+    score += result;
+    result = parseInt($("#meal").val());
+    score += result;
+    result = parseInt($("input:radio[name=vacation]:checked").val());
+    score += result;
+    result = parseInt($("input:radio[name=activity]:checked").val());
+    score += result;
+
+    if (score === 4){
+      $("#snakeIsland").slideDown();
+}
+    if (score > 4 && score < 8){
+      $("#yungasRoad").slideDown();
+}
+    if (score > 7 && score < 13){
+      $("#alnwickPoisonGardens").slideDown();
+}
+    if (score > 12 && score < 17){
+      $("#home").slideDown();
+}
+
+  var nameOutput = $("#name").val();
+    $(".greeting").show();
+    $(".name").text(nameOutput);
+
+    event.preventDefault();
+  });
+});
